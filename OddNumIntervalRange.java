6d6e05bbeea826fd2595248e060eece8a1ccee88
harshit6966 <harshit6966@gmail.com>
2022-06-13 12:37:08 +0530

    1523-LeetCode

---
+++ OddNumIntervalRange.java
@@ -0,0 +1,9 @@
+public class OddNumIntervalRange {
+    public int countOdds(int low, int high) {
+        if(high%2 == 0 && low%2 == 0) {
+            return (high-low)/2;
+        }else {
+            return (high-low)/2+1;
+        }
+    }
+}
