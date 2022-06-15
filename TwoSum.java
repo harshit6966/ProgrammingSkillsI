ddc58ba45a8a89f405ef3b3e5d1386fcf04617ce
harshit6966 <harshit6966@gmail.com>
2022-06-15 15:03:44 +0530

    1-LeetCode

---
+++ TwoSum.java
@@ -0,0 +1,19 @@
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int l = nums.length;
+        for(int i=0; i<l-1; i++) {
+            for(int j=i+1; j<l-1; j++) {
+                if(nums[i] + nums[j] == target)
+                    return new int[]{i+1, j+1};
+                return new int[]{-1,-1};
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,2};
+        int target = 3;
+        int res = twoSum(nums,target);
+        System.out.println("====" +res);
+    }
+}
