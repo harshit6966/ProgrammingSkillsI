73e7830b91565d82f439f45f1fed086c3c592998
harshit6966 <harshit6966@gmail.com>
2022-06-16 12:20:26 +0530

    Changes in 1-LeetCode

--- TwoSum.java
+++ TwoSum.java
@@ -1,21 +1,20 @@
+import java.util.Arrays;
+
 public class TwoSum {
-<<<<<<< HEAD
-    public int[] tagetSum(int[] nums, int target) {
-        for(int i=0; i<nums.length; i++) {
-            for(int j=i+1; j<nums.lenght; j++) {
+    public static int[] targetSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 int complement = target - nums[i];
-                if(nums[j] == complement) {
-                    return new int[] { i , j };
+                if (nums[j] == complement) {
+                    return new int[]{i, j};
                 }
             }
         }
-        throw IllegalArgumentException("No match found");
-=======
+        throw new IllegalArgumentException("No match found");
+    }
     public static void main(String[] args) {
-        int[] nums = {2,7,11,15};
+        int[] nums = {0,7,2,15};
         int target = 9;
-        int res = twoSum(nums, target);
-        System.out.println("====" +res);
->>>>>>> 3bcbcd7ff2f7fc996ec96acf53c90df83ea0e4b6
+        System.out.println(Arrays.toString(targetSum(nums, target)));
     }
 }
