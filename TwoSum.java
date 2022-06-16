947620dfbed678fa3300f0d4344ce3c3758394f9
harshit6966 <harshit6966@gmail.com>
2022-06-16 10:50:35 +0530

    1-LeetCode

--- TwoSum.java
+++ TwoSum.java
@@ -1,19 +1,13 @@
 public class TwoSum {
-    public int[] twoSum(int[] nums, int target) {
-        int l = nums.length;
-        for(int i=0; i<l-1; i++) {
-            for(int j=i+1; j<l-1; j++) {
-                if(nums[i] + nums[j] == target)
-                    return new int[]{i+1, j+1};
-                return new int[]{-1,-1};
+    public int[] tagetSum(int[] nums, int target) {
+        for(int i=0; i<nums.length; i++) {
+            for(int j=i+1; j<nums.lenght; j++) {
+                int complement = target - nums[i];
+                if(nums[j] == complement) {
+                    return new int[] { i , j };
+                }
             }
         }
-    }
-
-    public static void main(String[] args) {
-        int[] nums = {2,3,2};
-        int target = 3;
-        int res = twoSum(nums,target);
-        System.out.println("====" +res);
+        throw IllegalArgumentException("No match found");
     }
 }
